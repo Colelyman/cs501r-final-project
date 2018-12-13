c523454a55bfdc2c7564f2a9e21f55936c370a90
Cole Lyman <cole@colelyman.com>
2018-12-13 12:59:46 -0700

    Modified script to be more performant

--- image-segmentation/main.py
+++ image-segmentation/main.py
@@ -1,7 +1,4 @@
-!pip install torch torchvision opencv-python
-!pip install pillow==4.0.0
-
-# originally borrowed from https://github.com/kanezaki/pytorch-unsupervised-segmentation/blob/master/demo.py
+# adapted from https://github.com/kanezaki/pytorch-unsupervised-segmentation/blob/master/demo.py
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,6 +10,8 @@ import sys
 import numpy as np
 from skimage import segmentation
 import torch.nn.init
+import gc
+# import ipdb
 
 use_cuda = torch.cuda.is_available()
 
@@ -22,13 +21,13 @@ orig_img = cv2.imread(orig_img_path)
 empty_img = cv2.imread(empty_img_path)
 
 # Hyperparameters
-n_channel = 100
+n_channel = 30
 max_iter = 1000
-min_labels = 3
+min_labels = 2
 learning_rate = 0.1
 num_conv = 2 # the number of convolutional layers
 num_superpixels = 10000
-compactness = 100
+compactness = 10
 visualize = False
 
 # CNN model
@@ -37,11 +36,8 @@ class MyNet(nn.Module):
         super(MyNet, self).__init__()
         self.conv1 = nn.Conv2d(input_dim, n_channel, kernel_size=3, stride=1, padding=1 )
         self.bn1 = nn.BatchNorm2d(n_channel)
-        self.conv2 = []
-        self.bn2 = []
-        for i in range(num_conv-1):
-            self.conv2.append( nn.Conv2d(n_channel, n_channel, kernel_size=3, stride=1, padding=1 ) )
-            self.bn2.append( nn.BatchNorm2d(n_channel) )
+        self.conv2 = nn.ModuleList([nn.Conv2d(n_channel, n_channel, kernel_size=3, stride=1, padding=1) for _ in range(num_conv-1)])
+        self.bn2 = nn.ModuleList([nn.BatchNorm2d(n_channel) for _ in range(num_conv-1)])
         self.conv3 = nn.Conv2d(n_channel, n_channel, kernel_size=1, stride=1, padding=0 )
         self.bn3 = nn.BatchNorm2d(n_channel)
 
@@ -62,7 +58,6 @@ im = orig_img
 data = torch.from_numpy( np.array([im.transpose( (2, 0, 1) ).astype('float32')/255.]) )
 if use_cuda:
     data = data.cuda()
-data = Variable(data)
 
 # slic
 labels = segmentation.slic(im, compactness=compactness, n_segments=num_superpixels)
@@ -75,11 +70,8 @@ for i in range(len(u_labels)):
 # train
 model = MyNet( data.size(1) )
 if use_cuda:
-    model.cuda()
-    for i in range(num_conv-1):
-        model.conv2[i].cuda()
-        model.bn2[i].cuda()
-model.train()
+    model = model.cuda()
+
 loss_fn = torch.nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
 label_colours = np.random.randint(255,size=(100,3))
@@ -88,8 +80,8 @@ for batch_idx in range(max_iter):
     optimizer.zero_grad()
     output = model( data )[ 0 ]
     output = output.permute( 1, 2, 0 ).contiguous().view( -1, n_channel )
-    ignore, target = torch.max( output, 1 )
-    im_target = target.data.cpu().numpy()
+    _, target = torch.max( output, 1 )
+    im_target = target.detach().cpu().numpy()
     nLabels = len(np.unique(im_target))
     if visualize:
         im_target_rgb = np.array([label_colours[ c % 100 ] for c in im_target])
@@ -109,12 +101,13 @@ for batch_idx in range(max_iter):
     target = torch.from_numpy( im_target )
     if use_cuda:
         target = target.cuda()
-    target = Variable( target )
     loss = loss_fn(output, target)
+    gc.collect()
+    torch.cuda.empty_cache()
     loss.backward()
     optimizer.step()
 
-    print (batch_idx, '/', max_iter, ':', nLabels, loss.data[0])
+    print (batch_idx, '/', max_iter, ':', nLabels, loss.item())
     if nLabels <= min_labels:
         print ("nLabels", nLabels, "reached minLabels", min_labels, ".")
         break
@@ -123,7 +116,7 @@ for batch_idx in range(max_iter):
 if not visualize:
     output = model( data )[ 0 ]
     output = output.permute( 1, 2, 0 ).contiguous().view( -1, n_channel )
-    ignore, target = torch.max( output, 1 )
+    _, target = torch.max( output, 1 )
     im_target = target.data.cpu().numpy()
     im_target_rgb = np.array([label_colours[ c % 100 ] for c in im_target])
     im_target_rgb = im_target_rgb.reshape( im.shape ).astype( np.uint8 )
